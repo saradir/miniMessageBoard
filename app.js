403e9068a7c7671486914ee7a7e24b0ae4916e73
saradir <120840462+saradir@users.noreply.github.com>
2025-10-19 00:34:45 +0300

    change app to listen on process.env.PORT instead of hardcode

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('node:path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const indexRouter = require('./routers/indexRouter.js');
 
 
