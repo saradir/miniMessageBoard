e74631634dc1f2dddf801555f88ba76b0c304621
saradir <120840462+saradir@users.noreply.github.com>
2025-10-18 19:36:29 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('node:path');
+const app = express();
+const PORT = 3000;
+const indexRouter = require('./routers/indexRouter.js');
+
+
+
+app.set("views", path.join(__dirname,"views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/', indexRouter);
+
+
+
+app.listen(PORT, ()=> {
+    console.log(`Server running on http://localhost:${PORT}`);
+}
+)
