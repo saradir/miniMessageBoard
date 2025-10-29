d1290bb6fa4e598e057464e516d304864ddb4094
saradir <120840462+saradir@users.noreply.github.com>
2025-10-29 23:46:29 +0200

    add connection to render postgre db

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const path = require('node:path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const indexRouter = require('./routers/indexRouter.js');
-
+const { config } = require('dotenv');
+require('dotenv/config');
 
 
 app.set("views", path.join(__dirname,"views"));
